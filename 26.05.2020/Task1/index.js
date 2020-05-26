501d9e9c4c212aa30ef2ab1af0aad9024a99d56e
SonOfLion <syplusplus@gmail.com>
2020-05-26 10:48:06 +0300

    1

--- 26.05.2020/Task1/index.js
+++ 26.05.2020/Task1/index.js
@@ -28,26 +28,26 @@ function addPropertyV4(userData, userId) {
 
 }
 
-let userData = {
-    name: "Andrey",
-    city: "Lviv",
-    age: 20,
-};
+// let userData = {
+//     name: "Andrey",
+//     city: "Lviv",
+//     age: 20,
+// };
 
-let userId = "1111";
+// let userId = "1111";
 
-// let obj = {userId};
-console.log(addPropertyV4(userData, userId));
-// console.log(obj);
-console.log(userData);
+// // let obj = {userId};
+// console.log(addPropertyV4(userData, userId));
+// // console.log(obj);
+// console.log(userData);
 
-let userId = "1111";
+// let userId = "1111";
 
-console.log(userData);
+// console.log(userData);
 
-console.log(addPropertyV2(userData, userId));
+// console.log(addPropertyV2(userData, userId));
 
-console.log(userData);
+// console.log(userData);
 
 
 //DEMO
