6bab07b1fc11ab0c8f00a15ea8726e6e6664115e
SonOfLion <syplusplus@gmail.com>
2020-06-16 10:38:58 +0300

    1

--- Metods/Class/index.js
+++ Metods/Class/index.js
@@ -9,29 +9,30 @@ export function User(name, age) {
 
     this.name = name;
     this.age = age;
+
+    User.prototype.sayHi = function() {
+        console.log(`Hi, I am ${this.name}`)
+    };
+
+    User.prototype.requestNewPhoto = function() {
+        console.log(`New photo request was sent for ${this.name}`);
+    };
+
+    User.prototype.setAge = function(value) {
+        if (value < 0) {
+            return false;
+        }
+
+        this.age = value;
+
+        if (value >= 25) {
+            // console.log(this);
+            console.log(`New photo request was sent for ${this.name}`);
+        }
+        return value;
+    };
 }
 
-User.prototype.sayHi = function() {
-    console.log(`Hi, I am ${this.name}`)
-};
-
-User.prototype.requestNewPhoto = function() {
-    console.log(`New photo request was sent for ${this.name}`);
-};
-
-User.prototype.setAge = function(value) {
-    if (value < 0) {
-        return false;
-    }
-
-    this.age = value;
-
-    if (value >= 25) {
-        // console.log(this);
-        console.log(`New photo request was sent for ${this.name}`);
-    }
-    return value;
-};
 // console.log(User.prototype);
 // User.prototype.sayHi = 15;
 
