b4742ad86f4db1dcbb67be52825a1230cbf48f61
SonOfLion <syplusplus@gmail.com>
2020-06-22 10:08:40 +0300

    2

---
+++ Metods/Task15/index.js
@@ -0,0 +1,16 @@
+//DOM
+
+
+const setButton = (textButton) => {
+    //part 1
+    // const button = `<button>${textButton}</button>`;
+    // document.querySelector('body').innerHTML = button;
+
+    //part 2
+    const button = document.createElement('button');
+    button.textContent = textButton;
+
+    document.querySelector('body').append(button);
+};
+
+// console.log(typeoff(document));
