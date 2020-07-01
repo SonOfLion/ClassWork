c30233891f6f99f4ceb0d880b4e5ab83aa16a6f1
SonOfLion <syplusplus@gmail.com>
2020-07-01 11:55:17 +0300

    1

--- Metods/20/index.js
+++ Metods/20/index.js
@@ -1,49 +1,68 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1';
 
-export async function getUserData(userId) {
-    // let response;
+// export async function getUserData(userId) {
+// let response;
 
-    // try {
-    //     const response = await fetch(`${baseUrl}/users/${userId}`)
-    // } catch (error) {
-    //     debugger;
-    //     throw new Error('1111');
-    // }
+// try {
+//     const response = await fetch(`${baseUrl}/users/${userId}`)
+// } catch (error) {
+//     debugger;
+//     throw new Error('1111');
+// }
 
-    // .then(response => {
-    //     if (!response.ok) {
-    //         throw new Error('Can not find a user');
-    //     }
-    //     return response.json();
-    // });
+// .then(response => {
+//     if (!response.ok) {
+//         throw new Error('Can not find a user');
+//     }
+//     return response.json();
+// });
 
-    const response = await fetch(`${baseUrl}/users/${userId}`)
+//     const response = await fetch(`${baseUrl}/users/${userId}`)
 
-    if (!response.ok) {
-        throw new Error('Can not find a user');
-    }
+//     if (!response.ok) {
+//         throw new Error('Can not find a user');
+//     }
 
-    return response.json();
-}
+//     return response.json();
+// }
 
-export async function printUserData(userId) {
-    try {
-        const user = await getUserData(userId)
+// export async function printUserData(userId) {
+//     try {
+//         const user = await getUserData(userId)
 
-        console.log(user);
-    } catch (er) {
-        console.log(er);
-    }
-    // .then(userData => {
-    //     console.log(userData);
-    // })
-    // .catch(error => {
-    //     console.log(error.message);
-    // });
-}
+//         console.log(user);
+//     } catch (er) {
+//         console.log(er);
+// }
+// .then(userData => {
+//     console.log(userData);
+// })
+// .catch(error => {
+//     console.log(error.message);
+// });
+// }
 
 //Try
 //Catch
 //Throw
 
 // printUserData(13)
+
+export async function getUserData(userId) {
+    return await fetch(`${baseUrl}/users/${userId}`).then(response => {
+        if (!response.ok) {
+            throw new Error('Can not find a user');
+        }
+        return response.json();
+    });
+}
+
+export async function printUserData(userId) {
+    await getUserData(userId)
+        .then(userData => {
+            console.log(userData);
+        })
+        .catch(error => {
+            console.log(error.message);
+        });
+}
