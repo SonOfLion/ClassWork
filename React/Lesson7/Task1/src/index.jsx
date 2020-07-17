40834081268b3b8f502066311020a3a45f806e7e
SonOfLion <syplusplus@gmail.com>
2020-07-17 19:31:10 +0300

    1

--- React/Lesson7/Task1/src/index.jsx
+++ React/Lesson7/Task1/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import NumbersList from './NumberList';
+import NumbersList from './NumbersList';
 
 const rootElement = document.querySelector('#root');
 
