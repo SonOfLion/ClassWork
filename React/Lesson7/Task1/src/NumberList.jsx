975fb44ad7e863b97c12b5a67c45bdb99440656f
SonOfLion <syplusplus@gmail.com>
2020-07-17 19:23:57 +0300

    1

--- React/Lesson7/Task1/src/NumberList.jsx
+++ React/Lesson7/Task1/src/NumberList.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const NumberList = ({numbers}) => {
+const NumbersList = ({numbers}) => {
     const numbersElems = numbers.map(num => (<li>{num}</li>));
 
     return <ul>{numbersElems}</ul>;
 };
 
-export default NumberList;
+export default NumbersList;
--- React/Lesson7/Task1/src/index.jsx
+++ React/Lesson7/Task1/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import NumberList from './NumberList';
+import NumbersList from './NumberList';
 
 const rootElement = document.querySelector('#root');
 
 const numbers = [1,2,3,4,5];
 
-ReactDOM.render(<NumberList numbers={numbers} />, rootElement);
+ReactDOM.render(<NumbersList numbers={numbers} />, rootElement);
